707b4781ff20d631f1fb7e1df5453788f68731b4
gtrxAC <gtrxac2@gmail.com>
2023-05-21 23:36:36 +0300

    Added sort step

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Effort to analyze and clean up [J2ME archives](https://archive.org/download/J2ME
 [Original idea](https://docs.google.com/document/d/16Jjg64qgWixwob7FTYOnBwxEqv3hWZ4DfNn_DyWcpfk/edit?usp=sharing)
 
 ## Structure
-So far the procedure consists of 3 Python scripts:
+So far the procedure consists of 4 Python scripts:
 
 ### 1. Extract
 Recursively extract ZIP, 7Z and RAR archives.
@@ -18,3 +18,6 @@ We also need to check if the JARs are valid J2ME midlets, since some of them can
 Remove entries from the index that show signs of modification by third parties (such as pirate sites that put their own name on the manifest - we call these "bad keywords").
 
 At the same time, we also do de-duping of files based on the hashes.
+
+### 4. Sort
+Sort each JAR file into directories based on the app's name, and use a standard naming scheme, so variants of the same game are easy to find.
---
+++ sort.py
@@ -0,0 +1,58 @@
+# ______________________________________________________________________________
+#
+#  reJARchiver - sort.py
+#
+#  Moves the JARs to an output directory, sorted into folders by MIDlet name and
+#  named according to the naming scheme.
+# ______________________________________________________________________________
+#
+import os, sys, json, re
+
+def sort(indexname, folder):
+	os.makedirs(folder, exist_ok=True)
+	
+	with open(indexname) as indexfile:
+		illegal_chars = r'[\\/:\*\?"<>\|]'
+		periods_at_end = r'\.*$'
+		index = json.loads(indexfile.read())
+		
+		for hash, jar in index.items():
+			# Sanitize the fields for illegal chars just to be sure (won't be saved to the json)
+			if "MIDlet-Name" in jar: jar['MIDlet-Name'] = re.sub(illegal_chars, '', jar['MIDlet-Name'])
+			if "MIDlet-Vendor" in jar: jar['MIDlet-Vendor'] = re.sub(illegal_chars, '', jar['MIDlet-Vendor'])
+
+			# Make a folder for this MIDlet-Name (one folder can have many jars w same name)
+			# If already exists (another jar w same name has been scanned) that's fine
+			# Note: '.' at the end of a folder name isn't allowed?
+			path = os.path.join(folder, jar["MIDlet-Name"])
+			path = re.sub(periods_at_end, '', path)
+			os.makedirs(path, exist_ok=True)
+
+			# Compose the file name based on the naming scheme:
+			# Midlet-Name [Midlet-Vendor] (vX.Y.Z) {MainClass1, MainClass2} md5sum.jar
+			name = ""
+			if "MIDlet-Name" in jar: name += f"{jar['MIDlet-Name']}"
+			if "MIDlet-Vendor" in jar: name += f" [{jar['MIDlet-Vendor']}]"
+			if "MIDlet-Version" in jar: name += f" (v{jar['MIDlet-Version']})"
+
+			if "MIDlet-1" in jar and len(jar["MIDlet-1"].split(",")) > 2:
+				name += " {" + jar["MIDlet-1"].split(",")[2].strip()
+				
+			if "MIDlet-2" in jar and len(jar["MIDlet-2"].split(",")) > 2:
+				name += ", " + jar["MIDlet-2"].split(",")[2].strip()
+
+			name += "} " + f"{hash}.jar"
+			print(name)
+
+			# Move the first copy of the file to the output folder with the naming scheme
+			# Ignore failures (in case file is already moved)
+			try:
+				os.rename(jar["paths"][0], os.path.join(path, name))
+			except:
+				pass
+
+if __name__ == "__main__":
+	if len(sys.argv) < 3:
+		print("Usage: sort.py <filtered indexfile> <dest folder>")
+		sys.exit()
+	sort(sys.argv[1], sys.argv[2])
